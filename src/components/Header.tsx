6185d0b79e1ab56a25621f0e6959cc17ff4ffd56
SahilDurgia <sahildurgia44@gmail.com>
2026-03-27 16:51:16 +0530

    chore: rename dashboard to Emplytics for better branding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between">
       <h1 className="text-base xxs:text-lg xs:text-xl sm:text-2xl font-semibold text-gray-800">
-        FactWise Dashboard
+        TalentGrid
       </h1>
     </div>
   );
